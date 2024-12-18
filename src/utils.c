1664a89588ae89eacf358d576e7f495be1475e7a
ledias-d <leocraftjg@gmai.com>
2024-12-18 20:43:11 -0300

    refactor(utils):removed functions for reorganization in other files

--- src/utils.c
+++ src/utils.c
@@ -60,33 +60,6 @@ char	*ft_strdup(const char *s1)
 	return (ptr);
 }
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
-{
-	char	*ptr;
-	size_t	i;
-	size_t	s_len;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (ft_strdup(""));
-	if (len > s_len - start)
-		len = s_len - start;
-	i = 0;
-	ptr = malloc(len + 1);
-	if (!ptr)
-		return (NULL);
-	while (s[start] && i < len)
-	{
-		ptr[i] = s[start];
-		i++;
-		start++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
-}
-
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char			*ptr;
@@ -112,72 +85,29 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	return (ptr);
 }
 
-int	count_lines(char *file)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	int		fd;
-	int		lines;
-	char	*line;
+	char	*ptr;
+	size_t	i;
+	size_t	s_len;
 
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	lines = 0;
-	line = ft_strdup("");
-	while (line != NULL)
-	{
-		line = get_next_line(fd);
-		lines++;
-		free (line);
-	}
-	close (fd);
-	return (lines);
-}
-
-int	check_ber(const char *file)
-{
-	int	i;
-	int	j;
-
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
 	i = 0;
-	while (file[i])
-		i++;
-	i -= 4;
-	if (i < 0)
-		return (0);
-	j = 0;
-	while (".ber"[j])
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	while (s[start] && i < len)
 	{
-		if (file[i + j] != ".ber"[j])
-			return(0);
-		j++;
+		ptr[i] = s[start];
+		i++;
+		start++;
 	}
-	return (1);
-}
-
-int	check_rectangle(char **map)
-{
-	int	i;
-	
-}
-
-int	check_wall(char **map)
-{
-	
-}
-
-int	check_elements(char **map)
-{
-	
-}
-
-int	check_accessibility(char **map)
-{
-	
-}
-
-void	error_exit(const char *message)
-{
-	perror("Error");
-	write(1, message, ft_strlen(message));
-	exit(EXIT_FAILURE);
+	ptr[i] = '\0';
+	return (ptr);
 }
