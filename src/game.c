e8a2e34762c4e5ebd6690876bf64a8cc0bbf0864
ledias-d <leocraftjg@gmai.com>
2024-12-18 15:56:48 -0300

    feat: initial logic for the game (src/game.c)

---
+++ src/game.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ledias-d <ledias-d@student.42.rio>         #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-12-17 15:13:46 by ledias-d          #+#    #+#             */
+/*   Updated: 2024-12-17 15:13:46 by ledias-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+void	game_start(t_game *game, char **av)
+{
+	game->map = map_read(av[1]);
+	if (!game->map)
+		error_exit("Error reading the map");
+	if(!map_validate(game->map));
+		error_exit("Invalid map");
+	
+}
