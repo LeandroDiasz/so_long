fb43854bbdfc8b429516569c0ab2c07e91ecbf4f
Leandro Dias <leocraftjg@gmail.com>
2024-12-26 21:38:18 -0300

    refactor: moved free_matriz function to error.c file

--- src/erro.c
+++ src/erro.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   erro.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ledias-d <ledias-d@student.42.rio>         #+#  +:+       +#+        */
+/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-12-18 19:41:48 by ledias-d          #+#    #+#             */
-/*   Updated: 2024-12-18 19:41:48 by ledias-d         ###   ########.fr       */
+/*   Created: 2024/12/18 19:41:48 by ledias-d          #+#    #+#             */
+/*   Updated: 2024/12/26 21:21:56 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,19 @@ void	error_exit(const char *message)
 	perror("Error");
 	write(1, message, ft_strlen(message));
 	exit(EXIT_FAILURE);
+}
+
+void	free_matriz(char	**matriz)
+{
+	int	i;
+	
+	if (!matriz)
+		return ;
+	i = 0;
+	while (matriz[i])
+	{
+		free(matriz[i]);
+		i++;
+	}
+	free (matriz);	
 }
