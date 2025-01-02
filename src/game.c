91e646cdc0be0e4e61224cc99c36ce112cfb2fd8
Leandro Dias <leocraftjg@gmail.com>
2025-01-02 00:09:30 -0300

    refactor(game.c): changed the type of the game_start function

--- src/game.c
+++ src/game.c
@@ -6,17 +6,24 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:13:46 by ledias-d          #+#    #+#             */
-/*   Updated: 2024/12/26 21:36:39 by leo              ###   ########.fr       */
+/*   Updated: 2024/12/27 21:59:55 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void	game_start(t_game *game, char *file)
+int	game_start(t_game *game, char *file)
 {
 	game->map = map_read(file);
 	if (!game->map)
+	{
 		error_exit("Error reading the map");
+		return (0);
+	}
 	if(!map_validate(game->map))
+	{
 		error_exit("Invalid map");
+		return (0);
+	}
+	return (1);
 }
