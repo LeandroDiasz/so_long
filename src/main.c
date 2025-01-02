560515ab01c3014082b80d09c7bd7488dcab9855
Leandro Dias <leocraftjg@gmail.com>
2025-01-02 00:12:02 -0300

    feat(main.c): created structure to initialize the window

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ledias-d <ledias-d@student.42.rio>         #+#  +:+       +#+        */
+/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-12-16 15:55:45 by ledias-d          #+#    #+#             */
-/*   Updated: 2024-12-16 15:55:45 by ledias-d         ###   ########.fr       */
+/*   Created: 2024/12/16 15:55:45 by ledias-d          #+#    #+#             */
+/*   Updated: 2024/12/27 23:04:56 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	t_game game;
+	
+	if (ac != 2)
 	{
-		t_game game;
-		
-		game_start(&game, av[1]);
-	}
-	else
 		error_exit("Number of arguments invalid\n");
+		return (1);
+	}	
+	if(!game_start(&game, av[1]))
+		return (1);
+	game.mlx = mlx_init();
+	game.map_height = map_size(game.map);
+	game.map_width = ft_strlen(game.map[0]);
+	game.win = mlx_new_window(game.mlx, game.map_width * 64, \
+	game.map_height * 64, "so_long");
+	load_sprites(&game);
+	render_map(&game);
+	mlx_loop(game.mlx);
 	return (0);
 }
