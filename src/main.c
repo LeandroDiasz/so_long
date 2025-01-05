4589d0612d9dcdfc9b0b0a9263abbf51a2e6b098
ledias-d <leocraftjg@gmai.com>
2025-01-05 18:34:58 -0300

    feat: added hooks to main to capture keyboard input and close the window

--- src/main.c
+++ src/main.c
@@ -18,18 +18,19 @@ int	main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		error_exit("Number of arguments invalid\n");
+		ft_printf("Error: Invalid number of arguments\n");
 		return (1);
-	}	
+	}
 	if (!game_start(&game, av[1]))
 		return (1);
 	game.mlx = mlx_init();
-	game.map_height = map_size(game.map);
-	game.map_width = ft_strlen(game.map[0]);
 	game.win = mlx_new_window(game.mlx, game.map_width * 64, \
-	game.map_height * 64, "so_long");
+	game.map_height * 64, WIN_NAME);
 	load_sprites(&game);
 	render_map(&game);
+	mlx_hook(game.win, KeyPress, KeyPressMask, &handle_keypress, &game);
+	mlx_hook(game.win, DestroyNotify, StructureNotifyMask, \
+	&handle_close, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
