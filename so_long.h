a45b8e2343692663ca52efd46fca0c3afe650c6e
ledias-d <leocraftjg@gmai.com>
2025-01-06 19:28:46 -0300

    refactor: reorganize functions in so_long.h

--- so_long.h
+++ so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: ledias-d <ledias-d@student.42.rio>         #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/16 15:54:59 by ledias-d          #+#    #+#             */
-/*   Updated: 2024/12/27 21:50:48 by leo              ###   ########.fr       */
+/*   Created: 2025-01-06 21:32:54 by ledias-d          #+#    #+#             */
+/*   Updated: 2025-01-06 21:32:54 by ledias-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,21 +61,22 @@ typedef struct s_game
 	t_sprites	sprites;
 }			t_game;
 
+/*--- game.c ---*/
+int		game_start(t_game *game, char *file);
+
+/*--- map.c ---*/
+char	**map_read(char *file);
+int		map_validate(char **map);
+char	**map_generate(char **map, int fd);
+char	**dup_map(char **map);
+int		map_size(char **map);
+
 /*--- check_map.c ---*/
 int		check_elements(char **map);
 int		check_wall(char **map);
 int		check_rectangle(char **map);
 int		check_aux(char element, int *exit, int *player, int *collectibles);
-
-/*--- check_utils.c--c*/
-char	**dup_map(char **map);
-int		map_size(char **map);
-int		count_collectibles(char **map);
-int		validate_accessibility(char **map);
-
-/*--- erro.c ---*/
-void	error_exit(const char *message);
-void	free_matriz(char	**matriz);
+int		check_ber(const char *file);
 
 /* --- utils.c ---*/
 char	*ft_strchr(const char *s, int c);
@@ -94,15 +95,8 @@ int		count_lines(char *file);
 int		check_accessibility(char **map);
 void	dfs(char **map, int x, int y, int *collectibles);
 int		find_player(char **map, int *x, int *y);
-
-/*--- map.c ---*/
-char	**map_read(char *file);
-int		map_validate(char **map);
-int		check_ber(const char *file);
-char	**map_generate(char **map, int fd);
-
-/*--- game.c ---*/
-int		game_start(t_game *game, char *file);
+int		validate_accessibility(char **map);
+int		count_collectibles(char **map);
 
 /*--- events.c ---*/
 int move_player(t_game *game, int new_x, int new_y);
@@ -115,5 +109,9 @@ void	render_map(t_game *game);
 void	render_tile(t_game *game, int x, int y, char tile);
 void	load_sprites(t_game *game);
 void	load_sprite(void **sprite, char *path, t_game *game);
+
+/*--- erro.c ---*/
+void	error_exit(const char *message);
+void	free_matriz(char	**matriz);
 
 #endif
